a1807b02198d1077d77097a4bb4dca6fc2a434b0
Paramvir Singh <paramvir109@gmail.com>
2018-10-25 13:43:44 +0530

    Tests for GET/users/me and POST/users

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,62 @@
+const {ObjectID} = require('mongodb')
+const jwt = require('jsonwebtoken');
+
+
+const {Todo} = require('./../../models/todo')
+const {User} = require('./../../models/users')
+
+const myTodos = [
+    {
+        _id : new ObjectID(),
+        text : 'First test todo',
+        completed : true,
+        completedAt : 333
+    },
+    {
+        _id : new ObjectID(),
+        text : 'Second test todo'
+    }
+
+]
+const populateTodos = (done) => {//to empty the database before all assertions for(post)
+    Todo.deleteMany({}).then(() => {
+        return Todo.insertMany(myTodos)
+    }).then(() => done())
+
+}
+const user1ID = new ObjectID()
+const user2ID = new ObjectID()
+const myUser = [
+    {
+        _id : user1ID,
+        email : 'okay@ymail.com',
+        password : 'userpassone',
+        tokens : [
+            {
+            access : 'auth',
+            token : jwt.sign({_id : user1ID, access : 'auth'}, 'abc123').toString()
+            }
+        ]
+    },
+    {
+        _id : user2ID,
+        email : 'okay2@ymail.com',
+        password : 'userpasstwo',
+    }
+
+]
+const populateUsers = (done) => {//to empty the database before all assertions for(post)
+    User.deleteMany({}).then(() => {
+        let userOne = new User(myUser[0]).save()
+        let userTwo = new User(myUser[1]).save()//We use save instead of insert to call the middleware
+        
+       return Promise.all([userOne, userTwo])//Will simultaneously resolve the promise
+    }).then(() => done())
+
+}
+module.exports = {
+    myTodos,
+    populateTodos,
+    myUser,
+    populateUsers
+}
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,27 +6,10 @@ const {ObjectID} = require('mongodb')
 const app = require('./../server.js').app//Or use destructuring
 const {Todo} = require('./../models/todo')
 const {User} = require('./../models/users')
+const {myTodos, populateTodos,myUser,populateUsers} = require('./seed/seed')
 
-const myTodos = [
-    {
-        _id : new ObjectID(),
-        text : 'First test todo',
-        completed : true,
-        completedAt : 333
-    },
-    {
-        _id : new ObjectID(),
-        text : 'Second test todo'
-    }
-
-]
-
-beforeEach((done) => {//to empty the database before all assertions for(post)
-    Todo.deleteMany({}).then(() => {
-        return Todo.insertMany(myTodos)
-    }).then(() => done())
-
-})
+beforeEach(populateUsers)
+beforeEach(populateTodos)
 
 describe('POST /todos', () => {
     it('should create a new todo' , (done) => {
@@ -176,6 +159,81 @@ describe('PATCH todos/id', () => {
         .expect((res) => {
             expect(res.body.todo.completedAt).toNotExist();
         })
+        .end(done)
+        
+    })
+})
+describe('GET users/me' ,() => {
+    it('should return the user if authenticated', (done) => {
+        let token = myUser[0].tokens[0].token
+        request(app)
+        .get('/users/me')
+        .set('x-auth', token)//This is how we set the header
+        .expect(200)
+        .expect((res) => {
+            expect(res.body._id).toBe(myUser[0]._id.toHexString())
+            expect(res.body.email).toBe(myUser[0].email)
+        })
+        .end(done)
+    })
+    it('should return 401 if unauthenticated' , (done) => {
+        request(app)
+        .get('/users/me')
+        .expect(401)
+        .expect((res) => {
+            expect(res.body).toEqual({})
+        })
+        .end(done)
+    })
+})
+describe('POST /users' ,() => {
+    it('should add the user', (done) => {
+        let user = {
+            email : 'example@email.com',
+            password : 'example123'
+        }
+        request(app)
+        .post('/users')
+        .send(user)
+        .expect(200)
+        .expect((res) => {
+            expect(res.headers['x-auth']).toExist()
+            expect(res.body._id).toExist()
+            expect(res.body.email).toBe(user.email)
+        })
+        .end((err) => {
+            if(err) {
+               return done(err)
+            }
+            User.findOne({email : user.email}).then((doc) => {
+                expect(doc).toExist()
+                expect(doc.password).toNotEqual(user.password)
+                done()
+            })
+        })
+        
+    })
+    it('should return validation error if req invalid' , (done) => {
+        let user = {
+            email : 'exampleemailcom',//Invalid email or Invalid pass
+            password : 'example123'
+        }
+        request(app)
+        .post('/users')
+        .send(user)
+        .expect(400)
+        .end(done)
+        
+    })
+    it('should not create a user if email in use' , (done) => {
+        let user = {
+            email : myUser[0].email,//Invalid email
+            password : 'example123'
+        }
+        request(app)
+        .post('/users')
+        .send(user)
+        .expect(400)
         .end(done)
         
     })
